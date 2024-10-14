893fe8f2c01a6b43d5b8125db35c253b5b815286
manyxinaanna1 <142906388+manyxinaanna1@users.noreply.github.com>
2024-10-14 20:55:39 +0300

    телеграм-бот общаемся с чат gpt

--- src/main.js
+++ src/main.js
@@ -4,15 +4,49 @@ const ChatGptService = require("./gpt");
 class MyTelegramBot extends HtmlTelegramBot {
     constructor(token) {
         super(token);
+        this.mode = null;
     }
 
     async start(msg) {
+        this.mode = "main"
         const text = this.loadMessage("main")
         await this.sendImage("main")
         await this.sendText(text)
+
+         await this.showMainMenu(            {
+            "start": "Начать",
+             "profile": "генерация Tinder-профиля 😎",
+             "opener" : "сообщение для знакомства",
+             "message" : "переписка от вашего имени",
+            "date" : "переписка со звездами",
+            "gpt": "задать вопрос чату GPT",
+            "html": "Демонстрация HTML"
+            }
+        )
+    }
+
+    async html(msg) {
+        await this.sendHTML('<h3>Привет! </h3>')
+        const html = this.loadHtml("main")
+        await this.sendHTML(html, {theme: "dark"})
+    }
+    async gpt(msg) {
+        this.mode = "gpt"
+        const text = this.loadMessage("gpt")
+        await this.sendImage("gpt")
+        await this.sendText(text)
+    }
+
+    async gptDialog(msg) {
+        const text = msg.text;
+        const answer = await chatgpt.sendQuestion("Ответь на вопрос", text)
+        await this.sendText(answer)
     }
 
      async  hello(msg) {
+        if(this.mode === "gpt")
+        await this.gptDialog(msg);
+        else {
         const text = msg.text;
         await this.sendText("<b>Привет!</b>")
          await this.sendText("<i>Как дела?</i>")
@@ -24,6 +58,7 @@ class MyTelegramBot extends HtmlTelegramBot {
              "theme_light":"Светлая",
              "theme_dark":"Темная",
          })
+            }
     }
    async helloButton(callbackQuery) {
         const query = callbackQuery.data;
@@ -35,6 +70,10 @@ class MyTelegramBot extends HtmlTelegramBot {
 }
 
 const bot = new MyTelegramBot("7207101159:AAEj_ejzqcvhu1fqiz6iap4dlz1-lrEEwsU");
-bot.onCommand( /\/start/, bot.start)
-bot.onTextMessage(bot.hello)
-bot.onButtonCallback( /^.*/ , bot.helloButton )
+
+const chatgpt = new ChatGptService("gpt:ho7T8VOJeY6EVWR4okSxJFkblB3Tp7tf2SeYm4s7gNS3LPJN")
+bot.onCommand( /\/start/, bot.start);
+bot.onCommand( /\/html/, bot.html);
+bot.onCommand( /\/gpt/, bot.gpt);
+bot.onTextMessage(bot.hello);
+bot.onButtonCallback( /^.*/ , bot.helloButton );
